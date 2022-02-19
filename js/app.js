daf9108ede47a26276e8cd6b72a3d8d1c0036468
Bakyt Tynybekov <bakyt.tynybekov.ss@gmail.com>
2022-02-20 00:14:43 +0300

    added error message

--- js/app.js
+++ js/app.js
@@ -1,50 +1,46 @@
-const amount = document.getElementById('amount');
-const interest = document.getElementById('interest');
-const years = document.getElementById('years');
-const total = document.querySelector('.total');
-const totalInterest = document.querySelector('.totalInterest');
-const loader = document.getElementById('loader');
-const output = document.getElementById('output');
-const btn = document.querySelector('.btn');
-const amountOutput = document.querySelector('.amountOutput');
+const amount = document.getElementById("amount");
+const interest = document.getElementById("interest");
+const years = document.getElementById("years");
+const total = document.querySelector(".total");
+const totalInterest = document.querySelector(".totalInterest");
+const loader = document.getElementById("loader");
+const output = document.getElementById("output");
+const btn = document.querySelector(".btn");
+const amountOutput = document.querySelector(".amountOutput");
 
-
-btn.addEventListener('click', evaluate);
-
+btn.addEventListener("click", evaluate);
 
 function evaluate(e) {
+  e.preventDefault();
 
-    e.preventDefault();
+  let amountValue = +amount.value;
+  let interestValue = +interest.value;
+  let yearsValue = +years.value;
 
-    let amountValue = +amount.value;
-    let interestValue = +interest.value;
-    let yearsValue = +years.value;
-    amount.value = '';
-    interest.value = '';
-    years.value = '';
+  if (amountValue === 0 || interestValue === 0 || yearsValue === 0) {
+    alert("Заполните все поля!");
+  } else {
+    loader.style.display = "block";
+    setTimeout(loaderOff, 500);
+    setTimeout(outputF, 501);
+  }
 
+  amount.value = "";
+  interest.value = "";
+  years.value = "";
 
-    loader.style.display = 'block';
+  function loaderOff() {
+    loader.style.display = "none";
+  }
 
-     setTimeout(loaderOff, 500);
-
-     function loaderOff() {
-        loader.style.display = 'none';
-     }
-
-     setTimeout(outputF, 501);
-
-     function outputF() {
-         output.classList.add('output')
-        let monPer = interestValue / 1200;
-        console.log(monPer);
-        let ka = (monPer * (1 + monPer)**(yearsValue*12))/((1 + monPer)**(yearsValue*12) - 1)
-        amountOutput.value = Math.floor(ka * amountValue);
-        total.value = Math.floor(ka * amountValue * yearsValue * 12) ;
-        console.log(totalInterest);
-        totalInterest.value = Math.floor(ka * amountValue * yearsValue * 12) ;
-        console.log(Math.floor(ka * amountValue) * yearsValue * 12);
-     }
-   
-
+  function outputF() {
+    output.classList.add("output");
+    let monPer = interestValue / 1200;
+    let ka =
+      (monPer * (1 + monPer) ** (yearsValue * 12)) /
+      ((1 + monPer) ** (yearsValue * 12) - 1);
+    amountOutput.value = Math.floor(ka * amountValue);
+    total.value = Math.floor(ka * amountValue * yearsValue * 12);
+    totalInterest.value = Math.floor(ka * amountValue * yearsValue * 12);
+  }
 }
